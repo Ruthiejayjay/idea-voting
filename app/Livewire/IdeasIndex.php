24e8b0bf970d4cba5f2968e9ab92f242bc253d58
Ruth James <jamesruth449@gmail.com>
2024-01-03 21:42:22 +0100

    Search Filter

--- app/Livewire/IdeasIndex.php
+++ app/Livewire/IdeasIndex.php
@@ -16,11 +16,13 @@ class IdeasIndex extends Component
     public $status = 'All';
     public $category;
     public $filter;
+    public $search;
 
     protected $queryString = [
         'status',
         'category',
-        'filter'
+        'filter',
+        'search'
     ];
 
     protected $listeners = ['queryStringUpdatedStatus'];
@@ -31,6 +33,11 @@ class IdeasIndex extends Component
     }
 
     public function updatingFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingSearch()
     {
         $this->resetPage();
     }
@@ -64,16 +71,15 @@ class IdeasIndex extends Component
             'ideas' => Idea::with('user', 'category', 'status')
                 ->when($this->status && $this->status !== 'All', function ($query) use ($statuses) {
                     return $query->where('status_id', $statuses->get($this->status));
-                })
-                ->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
+                })->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
                     return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
-                })
-                ->when($this->filter && $this->filter === 'Top Voted', function ($query) {
+                })->when($this->filter && $this->filter === 'Top Voted', function ($query) {
                     return $query->orderByDesc('votes_count');
                 })->when($this->filter && $this->filter === 'My Ideas', function ($query) {
                     return $query->where('user_id', auth()->id());
-                })
-                ->addSelect([
+                })->when(strlen($this->search) >= 3, function ($query) {
+                    return $query->where('title', 'like', '%'.$this->search.'%');
+                })->addSelect([
                     'voted_by_user' => Vote::select('id')
                         ->where('user_id', auth()->id())
                         ->whereColumn('idea_id', 'ideas.id')
